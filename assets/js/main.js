13d6ee9b70bdebea406e90766d15fbab2f061b77
Glaucio Daniel <glauciodaniel@gmail.com>
2022-09-29 13:22:16 -0300

    'Deploy'

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//var nome; //hoisting - içar.
+
+console.log(nome, " chamando a variável usando o var");
+
+const nome = "Avanade";
+////////////////////////////////////////////////////////////////
+/**
+ * Existem 7 tipos de erros em JavaScript
+ * SyntaxError - Escreveu alguma coisa errado.
+ * ReferenceError - Chamou alguém que não existe.
+ *
+ */
