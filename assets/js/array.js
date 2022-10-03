d2a6eaa1e488cbf564c46e18c6c4e8c7b42ec5c1
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-03 14:00:17 -0300

    'Deploy'

--- assets/js/array.js
+++ assets/js/array.js
@@ -19,3 +19,20 @@ const carrosArray = [
 carrosArray.forEach((value, key) => {
   carros.innerHTML += `<option value="${key}">${value}</option>`;
 });
+
+carrosArray.forEach((value) => console.log(value));
+
+const importados = [
+  ["BMW", "320i", "M3", "M4", "X5", "X6", "z4 m40i"],
+  ["Audi", "a4", "s4", "s4 avanti", "Q7", "RS7", "e-Tron"],
+  ["Mercedes", "gtr", "e63", "s63", "brabus gle", "AMG G63"],
+];
+
+importados.forEach((marcas) => {
+  const select = document.createElement("select");
+  select.setAttribute("id", marcas[0]);
+  marcas.forEach((carros, key) => {
+    select.innerHTML += `<option value="${carros}">${carros}</option>`;
+  });
+  document.querySelector("body").appendChild(select);
+});
