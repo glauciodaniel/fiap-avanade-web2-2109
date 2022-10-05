303c5819985a8b466dc274d1b0293155af584215
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-05 14:16:23 -0300

    'Deploy'

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,20 +1,26 @@
 const cep = document.querySelector("#cep");
 
+const showData = (result) => {
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
+    }
+  }
+};
+
 //o evento blur significa que clicamos na caixa de texto e saímos dela.
 cep.addEventListener("blur", async (e) => {
   let search = cep.value.replace("-", "");
-
+  if (search < 8 || Number(search) === NaN) {
+    alert("CEP inválido");
+    return;
+  }
   const options = {
     method: "GET",
     mode: "cors",
     cache: "default",
   };
 
-  // https://viacep.com.br/ws/01001000/json/
-
-  //fetch vai gerar uma promise. Promise é uma promessa de que algo vai acontecer.
-  // se der certo, ele vai retornar o resultado. Se der errado, ele vai retornar um erro.
-  // se der certo nós resolvemos a promise. Se der errado, nós rejeitamos a promise.
   //Promise é uma intenção.
   const resultado = await fetch(
     `https://viacep.com.br/ws/${search}/json/`,
@@ -22,7 +28,7 @@ cep.addEventListener("blur", async (e) => {
   );
   const json = await resultado.json();
 
-  console.log(json);
+  showData(json);
 });
 
 // async/await
